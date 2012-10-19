c098ad1e73ee6e6be8eed429776e151995c76ef6
Keiichiro Ono <keiono@gmail.com>
2012-10-19 10:57:12 -0700

    Servlet sample had been added.

--- src/main/java/org/cytoscape/rest/Translator.java
+++ src/main/java/org/cytoscape/rest/Translator.java
@@ -2,7 +2,23 @@ package org.cytoscape.rest;
 
 import org.cytoscape.model.CyIdentifiable;
 
+/**
+ * Convert given Cytoscape object into other forms.
+ *
+ * @param <T> Converted object type.
+ * @param <V> Cytoscape Objects, i.e., CyNetwork, CyNode, CyEdge, or CyTable.
+ */
 public interface Translator<T, V extends CyIdentifiable> {
 
+	/**
+	 * Convert Cytoscape object into the specified data type.
+	 * The return value can be the following:
+	 *  - JSON (GraphSON)
+	 *  - XML
+	 *  - CSV
+	 *  
+	 * @param cytoscapeObject Original object to be converted into other object
+	 * @return Converted graph objects.
+	 */
 	T translate(final V cytoscapeObject);
 }

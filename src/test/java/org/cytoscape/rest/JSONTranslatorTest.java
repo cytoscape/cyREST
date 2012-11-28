184c8a17a7ea7ac6cdf4bca513ea22619932f5aa
Keiichiro Ono <keiono@gmail.com>
2012-11-28 13:42:47 -0800

    Test cases written in JavaScript had been added to unite tests.

---
+++ src/test/java/org/cytoscape/rest/JSONTranslatorTest.java
@@ -0,0 +1,32 @@
+package org.cytoscape.rest;
+
+import static org.junit.Assert.*;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public abstract class JSONTranslatorTest {
+	
+	protected ScriptEngine jsEngine;
+	protected Bindings bindings;
+
+	@Before
+	public void setUp() throws Exception {
+		final ScriptEngineManager manager = new ScriptEngineManager();
+		jsEngine = manager.getEngineByExtension("js");
+		
+		assertNotNull(jsEngine);
+		
+		bindings = jsEngine.createBindings();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+}

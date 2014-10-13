1f470f343b7b76c3939a909fdd83e5cccf13ceec
Keiichiro Ono <keiono@gmail.com>
2014-10-12 22:19:36 -0700

    Test cases for GET API for table have been updated.

--- src/test/java/org/cytoscape/rest/service/TableResourceTest.java
+++ src/test/java/org/cytoscape/rest/service/TableResourceTest.java
@@ -3,6 +3,7 @@ package org.cytoscape.rest.service;
 import static org.junit.Assert.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.ws.rs.core.Application;
@@ -26,7 +27,7 @@ public class TableResourceTest extends BasicResourceTest {
 	}
 	
 	@Test
-	public void testGetTables() throws Exception {
+	public void testGetTable() throws Exception {
 		final Long suid = network.getSUID();
 		String result = target("/v1/networks/" + suid.toString() + "/tables/defaultnode").request().get(
 				String.class);
@@ -40,7 +41,28 @@ public class TableResourceTest extends BasicResourceTest {
 	}
 
 	@Test
-	public void testGetColumns() throws Exception {
+	public void testGetTableAsCSV() throws Exception {
+		final Long suid = network.getSUID();
+		String result = target("/v1/networks/" + suid.toString() + "/tables/defaultnode.csv").request().get(
+				String.class);
+		assertNotNull(result);
+		final String[] rows = result.split("\n");
+		assertFalse(rows.length == 0);
+		final String header = rows[0];
+		final String[] columnNamesArray = header.split(",");
+		final Set<String> columnNames = new HashSet<>();
+		
+		for (String column : columnNamesArray) {
+			columnNames.add(column);
+		}
+		assertTrue(columnNames.contains("SUID"));
+		assertTrue(columnNames.contains("name"));
+		assertTrue(columnNames.contains("shared name"));
+		assertTrue(columnNames.contains("selected"));
+	}
+
+	@Test
+	public void testGetColumnNames() throws Exception {
 		final Long suid = network.getSUID();
 		String result = target("/v1/networks/" + suid.toString() + "/tables/defaultnode/columns").request().get(
 				String.class);
@@ -55,6 +77,19 @@ public class TableResourceTest extends BasicResourceTest {
 		assertTrue(columnNames.contains("name"));
 		assertTrue(columnNames.contains("shared name"));
 		assertTrue(columnNames.contains("selected"));
+	}
+	
+	
+	@Test
+	public void testGetColumn() throws Exception {
+		final Long suid = network.getSUID();
+		String result = target("/v1/networks/" + suid.toString() + "/tables/defaultnode/columns/name").request().get(
+				String.class);
+		assertNotNull(result);
+		final JsonNode root = mapper.readTree(result);
+		assertTrue(root.isObject());
+		assertTrue(root.get("values").isArray());
+		assertEquals("name", root.get("name").asText());
 	}
 
 
@@ -83,5 +118,27 @@ public class TableResourceTest extends BasicResourceTest {
 		final JsonNode root = mapper.readTree(result);
 		
 		assertTrue(root.isObject());
+	}
+
+
+	@Test
+	public void testGetCell() throws Exception {
+		final Long suid = network.getSUID();
+		final List<CyNode> nodes = network.getNodeList();
+		CyNode node = null;
+		
+		// Find target node for testing
+		for(CyNode n: nodes) {
+			if(network.getRow(n).get("name", String.class).equals("n1")) {
+				node = n;
+				break;
+			}
+		}
+		assertNotNull(node);
+		
+		String result = target("/v1/networks/" + suid.toString() + "/tables/defaultnode/rows/" + node.getSUID() + "/name").request().get(
+				String.class);
+		assertNotNull(result);
+		assertEquals("n1", result);
 	}
 }

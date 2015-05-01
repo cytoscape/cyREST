1146526ef68d6db882edde03f5bb3c53b1a57c2b
Keiichiro Ono <kono@5k.local>
2015-04-30 18:15:33 -0700

    UI resource updated

--- src/main/java/org/cytoscape/rest/internal/resource/UIResource.java
+++ src/main/java/org/cytoscape/rest/internal/resource/UIResource.java
@@ -2,18 +2,28 @@ package org.cytoscape.rest.internal.resource;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javassist.NotFoundException;
 
 import javax.inject.Singleton;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.application.swing.CytoPanel;
 import org.cytoscape.application.swing.CytoPanelName;
+
+import com.fasterxml.jackson.databind.deser.impl.ExternalTypeHandler.Builder;
 
 
 @Singleton
@@ -29,25 +39,45 @@ public class UIResource extends AbstractResource {
 	@GET
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public String getDesktop() {
-		return "{\"message\": \"Cytoscape Desktop is available.\"}";
+	public Map<String, String> getDesktop() {
+		final Map<String, String> status = new HashMap<>();
+		status.put("message", "GUI is available");
+		return status;
 	}
 
 	/**
 	 * 
 	 * @summary Get panel status 
 	 * 
-	 * @return Summary of server status
+	 * @return Panel status
 	 */
 	@GET
 	@Path("/panels")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public Map<String, String> getPanelStatus() {
-		final Map<String, String> status = new HashMap<>();
-		Arrays.asList(CytoPanelName.values()).stream()
+	public List<Map<String, String>> getAllPanelStatus() {
+		return Arrays.asList(CytoPanelName.values()).stream()
 			.map(panelName->desktop.getCytoPanel(panelName))
-			.forEach(panel->status.put(panel.getCytoPanelName().name(), panel.getState().name()));
-		return status;
+			.map(panel->getMap(panel)).collect(Collectors.toList());
 	}
-
+	
+	private final Map<String, String> getMap(final CytoPanel panel) {
+		final Map<String, String> values = new HashMap<>();
+		values.put("name", panel.getCytoPanelName().name());
+		values.put("state", panel.getState().name());
+		return values;
+	}
+	
+	
+	@GET
+	@Path("/panels/{panelName}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Response getPanelStatus(@PathParam("panelName") String panelName) {
+		final CytoPanelName panel = CytoPanelName.valueOf(panelName);
+		if(panel == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		
+		final CytoPanel panelObject = desktop.getCytoPanel(panel);
+		return Response.ok(getMap(panelObject)).build();
+	}
 }

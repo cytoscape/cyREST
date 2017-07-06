6290a9e450a866af8a988deb6612f57531952902
David Otasek <dotasek.dev@gmail.com>
2017-07-06 13:53:04 -0400

    BugFix: Columns results were skipping null values

--- src/main/java/org/cytoscape/rest/internal/serializer/GraphObjectSerializer.java
+++ src/main/java/org/cytoscape/rest/internal/serializer/GraphObjectSerializer.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.cytoscape.model.CyColumn;
@@ -134,12 +133,13 @@ public class GraphObjectSerializer {
 	public final String serializeColumnValues(final CyColumn column, final Collection<Object> values) throws IOException {
 
 		final JsonFactory factory = new JsonFactory();
-
+		
 		String result = null;
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 		JsonGenerator generator = null;
+	
 		generator = factory.createGenerator(stream);
-
+		
 		try {
 		generator.writeStartObject();
 		generator.writeStringField(JsonTags.COLUMN_NAME, column.getName());
@@ -245,6 +245,7 @@ public class GraphObjectSerializer {
 	private final void writeValue(final Class<?> type, final Object value, final JsonGenerator generator)
 			throws IOException {
 		if(value == null) {
+			generator.writeNull();
 			return;
 		}
 		

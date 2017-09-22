13b16bc179112ca62dc30aeef9a88578379f4a9e
David Otasek <dotasek.dev@gmail.com>
2017-09-21 20:10:01 -0400

    Added counter for successful requests.

--- src/main/java/org/cytoscape/rest/internal/resource/InstrumentationFilter.java
+++ src/main/java/org/cytoscape/rest/internal/resource/InstrumentationFilter.java
@@ -7,6 +7,8 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
+import io.prometheus.client.Counter;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,9 +17,30 @@ import org.slf4j.LoggerFactory;
 public class InstrumentationFilter implements ContainerResponseFilter {
 
 	private static final Logger logger = LoggerFactory.getLogger(InstrumentationFilter.class);
-
+	
+	public final static String[] LABELS = {
+		"METHOD",
+		"PATH"
+	};
+	
+	public final static String SUCCESSFUL_REQUESTS = "SUCCESSFUL_REQUESTS";
+	
+	static Counter requestCounter = Counter.build()
+			.name(SUCCESSFUL_REQUESTS).help("Successful requests to CyREST").labelNames(LABELS).register();
+	
 	@Override
 	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-			System.out.println("Instrumentation filter: " + request.getRequest().getMethod() + " " + request.getUriInfo().getPath());
+		try {
+			final String[] values = getValues(request);
+			requestCounter.labels(values).inc();
+			//System.out.println("Instrumentation filter: " + values[0] + " " + values[1] + " count=" + requestCounter.labels(values).get());
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.debug("Instrumentation filter produced an exception.", e);
+		}
+	}
+	
+	public String[] getValues(ContainerRequestContext request) {
+		return new String[]{request.getRequest().getMethod(), request.getUriInfo().getPath()};
 	}
 }

8c1de12e4ae44612a5bdef3469b6179d0bb6de3f
David Otasek <dotasek.dev@gmail.com>
2017-09-29 17:46:12 -0400

    Added instance label for push gateway push

--- src/main/java/org/cytoscape/rest/internal/resource/InstrumentationFilter.java
+++ src/main/java/org/cytoscape/rest/internal/resource/InstrumentationFilter.java
@@ -1,13 +1,22 @@
 package org.cytoscape.rest.internal.resource;
 
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.Map;
+
 import javax.annotation.Priority;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
+import io.prometheus.client.exporter.PushGateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +25,12 @@ import org.slf4j.LoggerFactory;
 @Priority(javax.ws.rs.Priorities.USER + 1)
 public class InstrumentationFilter implements ContainerResponseFilter {
 
-	private static final Logger logger = LoggerFactory.getLogger(InstrumentationFilter.class);
+	
+private static final Logger logger = LoggerFactory.getLogger(InstrumentationFilter.class);
+	
+	private static final PushGateway pg = new PushGateway("v1.prometheus-pushgateway.test.cytoscape.io:9091");
+	
+	private static final CollectorRegistry collectorRegistry = CollectorRegistry.defaultRegistry;
 	
 	public final static String[] LABELS = {
 		"METHOD",
@@ -28,12 +42,26 @@ public class InstrumentationFilter implements ContainerResponseFilter {
 	static Counter requestCounter = Counter.build()
 			.name(SUCCESSFUL_REQUESTS).help("Successful requests to CyREST").labelNames(LABELS).register();
 	
+	static RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
+	static String jvmName = runtimeBean.getName();
+	
+	static String hostAddress = "";
+	static {
+		try {
+			hostAddress = Inet4Address.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	static Map<String, String> groupingKey = Collections.singletonMap("instance", hostAddress+" "+jvmName);
+	
 	@Override
 	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
 		
 		try {
 			final String[] values = getValues(request);
 			requestCounter.labels(values).inc();
+			pg.push(collectorRegistry, SUCCESSFUL_REQUESTS, groupingKey);
 			//System.out.println("Instrumentation filter: " + values[0] + " " + values[1] + " count=" + requestCounter.labels(values).get());
 		} catch (Exception e) {
 			e.printStackTrace();

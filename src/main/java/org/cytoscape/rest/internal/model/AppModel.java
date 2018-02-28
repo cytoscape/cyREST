4e536b13e41b0d9161cfa2c77c237bccd96a85c5
Unknown <dotasek.dev@gmail.com>
2018-02-28 16:25:05 -0500

    Converted the allowableValues to ints, as they were improperly un-hexed

--- src/main/java/org/cytoscape/rest/internal/model/AppModel.java
+++ src/main/java/org/cytoscape/rest/internal/model/AppModel.java
@@ -12,6 +12,6 @@ public class AppModel {
 	public String bundleSymbolicName;
 	@ApiModelProperty(value="If this App is implemented as an [OSGi bundle](https://osgi.org/javadoc/r4v43/core/org/osgi/framework/Bundle.html), this field contains its `Bundle-Version` header", required=false)
 	public String bundleVersion;
-	@ApiModelProperty(value="If this App is implemented as an [OSGi bundle](https://osgi.org/javadoc/r4v43/core/org/osgi/framework/Bundle.html), this field contains its state.", required=false, allowableValues="1,2,4,8,10,20")
+	@ApiModelProperty(value="If this App is implemented as an [OSGi bundle](https://osgi.org/javadoc/r4v43/core/org/osgi/framework/Bundle.html), this field contains its state.", required=false, allowableValues="1,2,4,8,16,32")
 	public int bundleState;
 }

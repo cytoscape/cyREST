850e832e388e367036fe3687ef7245e9ad1eaf32
Unknown <dotasek.dev@gmail.com>
2018-03-27 14:40:28 -0400

    Fix for issues/74

--- src/test/java/org/cytoscape/rest/CyJSONUtilTest.java
+++ src/test/java/org/cytoscape/rest/CyJSONUtilTest.java
@@ -1,11 +1,17 @@
 package org.cytoscape.rest;
 
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -17,9 +23,11 @@ import java.util.Map;
 
 import org.cytoscape.rest.internal.CyJSONUtilImpl;
 import org.cytoscape.util.json.CyJSONUtil;
+import org.cytoscape.model.CyColumn;
 import org.cytoscape.model.CyIdentifiable;
 import org.cytoscape.model.CyNode;
 import org.cytoscape.model.CyRow;
+import org.cytoscape.model.CyTable;
 
 public class CyJSONUtilTest {
 	
@@ -78,5 +86,101 @@ public class CyJSONUtilTest {
 		String jsonString = jsonUtil.toJson(row);
 		JsonElement rootElement = parser.parse(jsonString);
 		System.out.println(jsonString);
+	}
+	
+	@Test
+	public void getCyColumnPrimitiveTest() {
+		String dummyColumnName = "dummyColumn";
+		CyColumn cyColumn = mock(CyColumn.class);
+		try {
+			doAnswer(new Answer<Class<?>>() {
+				public Class<?> answer(InvocationOnMock invocation) {
+					return String.class;
+				}
+			}).when(cyColumn).getType();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		when(cyColumn.getName()).thenReturn(dummyColumnName);
+		CyRow row = mock(CyRow.class);
+		
+		when(row.get(dummyColumnName, String.class)).thenReturn("dummyStringValue");
+	
+		//List<String> dummyStringList = Arrays.asList("Hey", "Ho");
+		
+		//dummyMap.put("booleanCol", true);
+		//dummyMap.put("listCol", dummyStringList);
+		
+		CyTable table = mock(CyTable.class);
+		when(table.getAllRows()).thenReturn(Arrays.asList(new CyRow[] {row}));
+		
+		when(cyColumn.getTable()).thenReturn(table);
+		
+		String jsonString = jsonUtil.toJson(cyColumn, true, true);
+		System.out.println(jsonString);
+		
+		JsonElement rootElement = parser.parse(jsonString);
+		assertEquals("String", rootElement.getAsJsonObject().get("type").getAsString());
+		
+		JsonElement values = rootElement.getAsJsonObject().get("values");
+		assertTrue(values.isJsonArray());
+		assertEquals(1, values.getAsJsonArray().size());
+		assertEquals("dummyStringValue", values.getAsJsonArray().get(0).getAsString());
+	
+	}
+	
+	@Test
+	public void getCyColumnListTest() {
+		String dummyColumnName = "dummyColumn";
+		CyColumn cyColumn = mock(CyColumn.class);
+		try {
+			doAnswer(new Answer<Class<?>>() {
+				public Class<?> answer(InvocationOnMock invocation) {
+					return List.class;
+				}
+			}).when(cyColumn).getType();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			doAnswer(new Answer<Class<?>>() {
+				public Class<?> answer(InvocationOnMock invocation) {
+					return String.class;
+				}
+			}).when(cyColumn).getListElementType();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		
+		
+		when(cyColumn.getName()).thenReturn(dummyColumnName);
+		CyRow row = mock(CyRow.class);
+		when(row.get(dummyColumnName, List.class)).thenReturn(Arrays.asList(new String[] {"a", "b"}));
+	
+		//List<String> dummyStringList = Arrays.asList("Hey", "Ho");
+		
+		//dummyMap.put("booleanCol", true);
+		//dummyMap.put("listCol", dummyStringList);
+		
+		CyTable table = mock(CyTable.class);
+		when(table.getAllRows()).thenReturn(Arrays.asList(new CyRow[] {row}));
+		
+		when(cyColumn.getTable()).thenReturn(table);
+		
+		String jsonString = jsonUtil.toJson(cyColumn, true, true);
+		System.out.println(jsonString);
+		
+		JsonElement rootElement = parser.parse(jsonString);
+		assertEquals("List", rootElement.getAsJsonObject().get("type").getAsString());
+		assertEquals("String", rootElement.getAsJsonObject().get("listType").getAsString());
+		JsonElement values = rootElement.getAsJsonObject().get("values");
+		assertTrue(values.isJsonArray());
+		assertEquals(1, values.getAsJsonArray().size());
+		JsonArray array = values.getAsJsonArray().get(0).getAsJsonArray();
+		assertEquals(2, array.size());
+		assertEquals("a", array.get(0).getAsString());
+		assertEquals("b", array.get(1).getAsString());
 	}
 }

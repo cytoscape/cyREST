608f9245b4649717afbe742f3e6d60805ea0ef67
D <dotasek.dev@gmail.com>
2019-09-18 15:36:31 -0700

    Update develop versions for Cytoscape 3.8

--- src/test/java/org/cytoscape/rest/service/StyleResourceTest.java
+++ src/test/java/org/cytoscape/rest/service/StyleResourceTest.java
@@ -88,7 +88,7 @@ public class StyleResourceTest extends BasicResourceTest {
 		final JsonNode root = mapper.readTree(body);
 		assertTrue(root.get("defaults").isArray());
 		final JsonNode defaults = root.get("defaults");
-		assertEquals(109, defaults.size());
+		assertEquals(110, defaults.size());
 	}
 
 
@@ -135,7 +135,7 @@ public class StyleResourceTest extends BasicResourceTest {
 		System.out.println(body);
 		final JsonNode root = mapper.readTree(body);
 		assertTrue(root.isArray());
-		assertEquals(109, root.size());
+		assertEquals(110, root.size());
 	}
 
 	@Test
@@ -147,7 +147,7 @@ public class StyleResourceTest extends BasicResourceTest {
 		System.out.println(body1);
 		final JsonNode root1 = mapper.readTree(body1);
 		assertTrue(root1.isArray());
-		assertEquals(109, root1.size());
+		assertEquals(110, root1.size());
 
 		//assertEquals(root1, root);
 

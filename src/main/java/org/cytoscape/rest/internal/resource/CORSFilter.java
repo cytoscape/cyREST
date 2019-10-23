d0bc2b8a46e48bda7fffe28ed427b5f1f194d25e
D <dotasek.dev@gmail.com>
2019-10-23 14:52:31 -0700

    Don't allow credentials on OPTIONS

--- src/main/java/org/cytoscape/rest/internal/resource/CORSFilter.java
+++ src/main/java/org/cytoscape/rest/internal/resource/CORSFilter.java
@@ -17,7 +17,7 @@ public class CORSFilter implements ContainerResponseFilter {
 	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
 		response.getHeaders().add("Access-Control-Allow-Origin", "*");
 		response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-		response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+		response.getHeaders().add("Access-Control-Allow-Credentials", request.getMethod().equals("OPTIONS") ? "false" : "true");
 		response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 	}
 }

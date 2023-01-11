4f9845cdbc4dbc32cab9aff8ff58f1a6f447dde8
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2023-01-10 23:55:57 -0800

    Fix CYTOSCAPE-13022

--- src/main/java/org/cytoscape/rest/internal/CyAutomationAction.java
+++ src/main/java/org/cytoscape/rest/internal/CyAutomationAction.java
@@ -6,27 +6,22 @@ import java.net.URISyntaxException;
 
 import org.cytoscape.application.swing.AbstractCyAction;
 import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.util.swing.OpenBrowser;
 
 public class CyAutomationAction extends AbstractCyAction{
 
 	private final CyServiceRegistrar serviceRegistrar;
-	
+
 	public CyAutomationAction(CyServiceRegistrar serviceRegistrar) {
 		super("Tutorials");
 		this.setPreferredMenu(CyRESTConstants.CY_REST_HELP_MENU_ANCHOR);
 		this.serviceRegistrar = serviceRegistrar;
-	
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-	
-		try {
-			DesktopBrowseUtil.browse("http://automation.cytoscape.org");
-		} catch (IOException | URISyntaxException e1) {
-			e1.printStackTrace();
-		}
+		final OpenBrowser openBrowser = serviceRegistrar.getService(OpenBrowser.class);
+		openBrowser.openURL("http://automation.cytoscape.org");		
 	}
-
-	
 }
